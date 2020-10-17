5d3fd1ed75e99f36b92a6e1c9ebd3b21b9b85058
Kasper Sogaard <kasper.sogaard@hulu.com>
2020-10-16 20:28:49 -0700

    Don't copy the git ignore

--- BuildScript.cmd
+++ BuildScript.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 IF NOT EXIST .\build mkdir .\build
-robocopy . .\build /E /NFL /NJH /NJS /NDL /NC /NS /NP /XF "%CD%\ISLAND" /XF "%CD%\BuildScript" /XF "%CD%\BuildScript.cmd" /XD "%CD%\build" /XD "%CD%\.git"
+robocopy . .\build /E /NFL /NJH /NJS /NDL /NC /NS /NP /XF "%CD%\ISLAND" /XF "%CD%\BuildScript" /XF "%CD%\BuildScript.cmd" /XD "%CD%\build" /XD "%CD%\.git" /XF "%CD%\.gitignore"
 exit 0
